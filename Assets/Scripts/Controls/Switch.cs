cbc1b317747a910f3765a754b98be3980f6d1064
Alec Gerchman <arg2502@rit.edu>
2018-09-10 21:05:00 -0400

    Adding in functionality for the 2 switches

---
+++ Assets/Scripts/Controls/Switch.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : Control {
+
+    Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        animator.speed = 0;
+        UpdateSwitchPosition();
+    }
+
+    protected override void OnMouseDown()
+    {
+        base.OnMouseDown();
+
+        if (value <= MIN_VALUE)
+            value = NEUTRAL;
+        else if (value == NEUTRAL)
+            value = MAX_VALUE;
+        else if (value >= MAX_VALUE)
+            value = MIN_VALUE;
+
+        UpdateSwitchPosition();
+    }
+
+    void UpdateSwitchPosition()
+    {
+        var time = (value + 1) / 2f;
+        if (time >= 1f) time = 0.99f;
+        var stateName = "Switch";
+        if (gameObject.name.Contains("B"))
+            stateName += "B";
+        animator.Play(stateName, -1, time);
+    }
+
+}
