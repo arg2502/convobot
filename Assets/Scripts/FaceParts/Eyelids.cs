faf009dd90dd65f44f75acb4b4c8ed0357c44e36
Alec Gerchman <arg2502@rit.edu>
2018-09-10 22:14:17 -0400

    Fixing eyelids to rotate around eye using animations

--- Assets/Scripts/FaceParts/Eyelids.cs
+++ Assets/Scripts/FaceParts/Eyelids.cs
@@ -4,29 +4,33 @@ using UnityEngine;
 
 public class Eyelids : FacePart {
 
-    public GameObject lowerlid;
-    public GameObject upperlid;
+    //public GameObject lowerlid;
+    //public GameObject upperlid;
 
-    float openPos;
-    float closedPos = 0.5f;
-    float middleYPos;
+    Animator animator;
+
+    //float openPos;
+    //float closedPos = 0.5f;
+    //float middleYPos;
 
     public enum EyelidState { OPEN, SQUINTING, CLOSED }
     public EyelidState eyelidState;
 
 	void Start () {
-        openPos = upperlid.transform.localPosition.y;
+        //openPos = upperlid.transform.localPosition.y;
 
-        // set eyelids to halfway
-        var middle = (openPos + closedPos) / 2f;
+        //// set eyelids to halfway
+        //var middle = (openPos + closedPos) / 2f;
 
-        var newUpper = upperlid.transform.localPosition;
-        newUpper.y = middle;
-        upperlid.transform.localPosition = newUpper;
-        newUpper.y *= -1;
-        lowerlid.transform.localPosition = newUpper;
+        //var newUpper = upperlid.transform.localPosition;
+        //newUpper.y = middle;
+        //upperlid.transform.localPosition = newUpper;
+        //newUpper.y *= -1;
+        //lowerlid.transform.localPosition = newUpper;
 
-        middleYPos = upperlid.transform.localPosition.y;
+        //middleYPos = upperlid.transform.localPosition.y;
+
+        animator = GetComponent<Animator>();
 
 	}
 	
@@ -39,11 +43,19 @@ public class Eyelids : FacePart {
 
     void MoveLids()
     {
-        var newPos = upperlid.transform.localPosition;
-        newPos.y = middleYPos + (value * (middleYPos - closedPos)); // some math was done to limit the lids to their open/closed positions
-        upperlid.transform.localPosition = newPos;
-        newPos.y *= -1;
-        lowerlid.transform.localPosition = newPos;
+        //var newPos = upperlid.transform.localPosition;
+        //newPos.y = middleYPos + (value * (middleYPos - closedPos)); // some math was done to limit the lids to their open/closed positions
+        //upperlid.transform.localPosition = newPos;
+        //newPos.y *= -1;
+        //lowerlid.transform.localPosition = newPos;
+
+        var time = (value + 1) / 2f;
+        if (time >= 1f) time = 0.99f;
+        else if (time <= 0f) time = 0f;
+        var stateName = "Eyelid";
+        if (gameObject.name.Contains("Right"))
+            stateName += "R";
+        animator.Play(stateName, -1, time);
     }
 
     void CheckState()
