777ccf80886c7730597f604bbe50ef524e8eb24f
kevaaan <kevandupont@gmail.com>
2018-12-05 00:27:16 -0500

    Exported Robot with new Eyelids and Set up the new Robot in the game scene

--- Assets/Scripts/FaceParts/Eyelids.cs
+++ Assets/Scripts/FaceParts/Eyelids.cs
@@ -7,28 +7,43 @@ public class Eyelids : FacePart {
     //public GameObject lowerlid;
     //public GameObject upperlid;
 
-    Animator animator;
+    //Animator animator;
 
     //float openPos;
     //float closedPos = 0.5f;
     //float middleYPos;
 
+    SkinnedMeshRenderer smr;
+
     public enum EyelidState { OPEN, SQUINTING, CLOSED, _COUNT }
     public EyelidState eyelidState;
 
 	void Start () {       
-        animator = GetComponent<Animator>();
+        //animator = GetComponent<Animator>();
+        smr = GetComponentInChildren<SkinnedMeshRenderer>();
         numOfStates = (int)EyelidState._COUNT;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        MoveLids();
+        //MoveLids();
+        CheckBlendState();
         CheckState();
 	}
 
-    void MoveLids()
+    void CheckBlendState()
+    {
+        // 0 = frown
+        // 50 = neutral
+        // 100 = smile
+
+        // get the value, add 1, and multiply by 50 -- to translate to Blend State
+        var newBlendState = (value + 1f) * 50f;
+        smr.SetBlendShapeWeight(0, newBlendState); // 1 for open state
+    }
+
+    /*void MoveLids()
     {
         //var newPos = upperlid.transform.localPosition;
         //newPos.y = middleYPos + (value * (middleYPos - closedPos)); // some math was done to limit the lids to their open/closed positions
@@ -43,15 +58,21 @@ public class Eyelids : FacePart {
         if (gameObject.name.Contains("Right"))
             stateName += "R";
         animator.Play(stateName, -1, time);
-    }
+    }*/
 
     void CheckState()
     {
-        if (value > 0.6f)
+        if (value <= -0.9f)
+            eyelidState = EyelidState.OPEN;
+        else if (value > -0.9f && value <= 0.5f)
+            eyelidState = EyelidState.SQUINTING;
+        else
+            eyelidState = EyelidState.CLOSED;
+        /*if (value > 0.6f)
             eyelidState = EyelidState.OPEN;
         else if (value <= -1f)
             eyelidState = EyelidState.CLOSED;
         else
-            eyelidState = EyelidState.SQUINTING;
+            eyelidState = EyelidState.SQUINTING;*/
     }
 }
