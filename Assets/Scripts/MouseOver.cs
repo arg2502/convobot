21e43f9ed29df86843313ae51b76a8a33ecf80dc
rachelmakesgames <rachelmakesgames@gmail.com>
2019-06-20 14:31:15 -0500

    Adds mouseover scaling

---
+++ Assets/Scripts/MouseOver.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseOver : MonoBehaviour
+{
+    public float scaleUpSizeModifier = 1.1f;
+    public void OnMouseEnter()
+    {
+        Debug.Log("here");
+        transform.localScale += new Vector3(scaleUpSizeModifier, scaleUpSizeModifier, scaleUpSizeModifier); 
+    }
+
+
+    public void OnMouseExit()
+    {
+        Debug.Log("there");
+        transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);  
+    }
+
+}
